5c6df5b1d9eabc2a715cf613c73fc477a33bafbd
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-19 10:19:35 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,6 +100,6 @@ output "k8s_master_ssh" {
 }
 
 output "k8s_worker_ssh" {
-  value = zipmap( values(aws_instance.worker).[*].name, values(aws_instance.worker).[*].public_dns )
- 
+  value = aws_instance.worker[0].public_dns
+  value = aws_instance.worker[1].public_dns
 }
