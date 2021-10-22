c53067d98b8d584a4035b5d979b18badfe5f19f4
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-22 12:08:39 -0300

    Update deploy.sh

---
+++ nginx/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd terraform
+terraform init
+terraform fmt
+terraform apply -auto-approve
+
+echo  "Aguardando a criação das maquinas ..."
+sleep 5
+
+ID_M1=$(~/terraform/terraform output | grep 'k8s-master 1 -' | awk '{print $4;exit}')
+ID_M1_DNS=$(~/terraform/terraform output | grep 'k8s-master 1 -' | awk '{print $9;exit}' | cut -b 8-)
+
+
+echo "
+[ec2-k8s-proxy]
+$ID_HAPROXY_DNS
+
+" > ../ansible/hosts
+
+
+
+cd ../ansible
+ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /home/ubuntu/.ssh/id_rsa
