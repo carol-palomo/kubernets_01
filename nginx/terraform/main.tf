56957e6b484747e9774d1b13e9199f3904e3ee40
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-22 12:12:09 -0300

    Update main.tf

--- nginx/terraform/main.tf
+++ nginx/terraform/main.tf
@@ -86,8 +86,6 @@ resource "aws_security_group" "acessos_nginx" {
 
 # terraform refresh para mostrar o ssh
 output "aws_instance_e_ssh" {
-  value = [
-    aws_instance.nginx.public_ip,
-    "sudo ssh -i /root/.ssh/id_rsa ubuntu@${aws_instance.nginx.public_dns}"
-  ]
+ value = ${aws_instance.nginx.public_dns}
+
 }
