1acaaf32e511cbf28be221270aa2f45fffd33f64
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-22 15:03:09 -0300

    Update deploy.sh

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -10,16 +10,16 @@ echo  "Aguardando a criação das maquinas ..."
 sleep 5
 
 ID_M1_DNS=$(terraform output | cut -b 23- | rev | cut -b 2- | rev)
-
+echo ID_M1_DNS
 
 sudo echo "
 [ec2-nginx]
 $ID_M1_DNS
 
-" > ../ansible/hosts
+" > /var/lib/jenkins/workspace/PipelineInfra/nginx/ansible/hosts
 
 echo  "Aguardando ..."
 sleep 5
 
-cd ../ansible
+cd ../var/lib/jenkins/workspace/PipelineInfra/nginx/ansible
 ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /home/ubuntu/.ssh/id_rsa
